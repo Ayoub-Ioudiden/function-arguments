834efd6222512f98d11bcfa07c86f0ec47d8dc42
Ayoub-Ioudiden <93585615+Ayoub-Ioudiden@users.noreply.github.com>
2022-01-06 14:44:26 +0100

    arrays

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const paintWall = function (item) {
-    console.log("The wall has been painted " + item);
-
+//Object
+const person = {
+    name: "Joopie",
+    age: 26,
+    evaluations: [7, 10, 9]
 };
 
-paintWall("green");
+console.log(person);
 
-const paintWall1 = function (item) {
-    console.log(item);
+console.log(person.name);
+console.log(person['name']);
 
-};
+console.log(person.age);
+console.log(person['age']);
 
-const northwall = "the north wall has been painted orange ";
-const southEastWall = "the south-east wall has been painted grey";
+console.log(person.evaluations);
 
 
-paintWall1(northwall + southEastWall);
+//Array
+
+const selectedColors = ['green', 'blue', 'red'];
+console.log(selectedColors);
+
+console.log(selectedColors.length);
+
+console.log(selectedColors[0]);
+
+console.log(selectedColors[2]);
+
+console.log(selectedColors.push('yellow'));
+
+console.log(selectedColors.push(5));
+
+console.log(selectedColors.push({ greeting: "hi, i am an object" }));
+
+console.log(selectedColors[5]);
+
